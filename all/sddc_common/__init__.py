599a265fd60d265c37cd49ab677dc0abc2699098
Stuart Esdaile <stuart.esdaile@sydney.edu.au>
2018-02-27 20:46:24 +1100

    Added updates

--- all/sddc_common/__init__.py
+++ all/sddc_common/__init__.py
@@ -1,2 +1,2 @@
-from async import AsyncCmd, AsyncMacroCmd, AsyncMacro, update_state
-import git
+from .async import AsyncCmd, AsyncMacroCmd, AsyncMacro, update_state, AsyncMacroRunner
+from .workflow import work_on, deploy_for, promote_for, promote_to, switch_to, finalize_for, sync_for
