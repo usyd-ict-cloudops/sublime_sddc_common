ab6a216d1c616175d5527717ef23b8e0055a89cd
Stuart Esdaile <stuart.esdaile@sydney.edu.au>
2018-03-06 03:14:46 +1100

    Added Smart Install Command

--- all/sddc_common/__init__.py
+++ all/sddc_common/__init__.py
@@ -1,2 +1,3 @@
 from .async import AsyncCmd, AsyncMacroCmd, AsyncMacro, update_state, AsyncMacroRunner
+from .installer import SmartInstallCommand
 from .workflow import work_on, deploy_for, promote_for, promote_to, switch_to, finalize_for, sync_for
